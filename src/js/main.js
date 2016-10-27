402ddafdbd46168471eea224477c6b7abb5f13de
Jordan Carroll <jfindlayc@gmail.com>
2016-10-27 15:22:05 -0400

    Implemented expand button event

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 function init() {
+
 	renderClock();
+
+	// execute http request to pull data
 	$.ajax({
 		url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=Nagasaki,392&mode=json&cnt=16&APPID=f6e829e9fecf2ba3637d0eed96a2ce85",
 		success: function (results) {
@@ -9,8 +12,14 @@ function init() {
 }
 
 function buildApp (results) {
+	// Add city name to page
+	$("#city-name").html(results.city.name);
+
+	// Generate and render the AppView
 	var report = new AppView(results, "div");
-report.render();
+	report.render();
+
+	// Append the AppView to the body
 	$(document.body).append(report.element);
 }
 
